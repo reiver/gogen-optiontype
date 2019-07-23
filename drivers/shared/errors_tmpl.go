3b575c6d821a1a1173e38122513861a0f65c429e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-07-23 13:35:53 -0700

    tweaked text for errors

--- drivers/shared/errors_tmpl.go
+++ drivers/shared/errors_tmpl.go
@@ -10,10 +10,10 @@ const (
 	ErrorsTmpl =
 `
 var (
-	errNilReceiver     = errors.New("{{.Pkg}: Nil Receiver")
-	errNothing         = errors.New("{{.Pkg}: {{.Pkg}}.Nothing()")
-	errNothingNullable = errors.New("{{.Pkg}: {{.Pkg}}.NothingNullable()")
-	errNull            = errors.New("{{.Pkg}: {{.Pkg}}.Null()")
+	errNilReceiver     = errors.New("{{.Pkg}}: Nil Receiver")
+	errNothing         = errors.New("{{.Pkg}}: {{.Pkg}}.Nothing()")
+	errNothingNullable = errors.New("{{.Pkg}}: {{.Pkg}}.NothingNullable()")
+	errNull            = errors.New("{{.Pkg}}: {{.Pkg}}.Null()")
 )
 `
 )
